c4a1068ec742427488d160b1811f660e584c979f
92023 <92023@farnborough.ac.uk>
2020-10-15 12:01:40 +0100

    need to fix user get api for html/thinge

--- resources/client/js/users.js
+++ resources/client/js/users.js
@@ -22,6 +22,24 @@ function formatUsersList(myJSONArray){
     }
     document.getElementById("UsersTable").innerHTML = dataHTML;
 }
+function getUser() {
+    console.log("Invoked getUser()");     //console.log your BFF for debugging client side
+    const UserID = document.getElementById("UserID").value;  //get the UserId from the HTML element with id=UserID
+    //let UserID = 1; 			  //You could hard code it if you have problems
+    //debugger;				  //debugger statement to allow you to step through the code in console dev F12
+    const url = "/users/getUser/";       // API method on webserver
+    fetch(url + UserID, {                // UserID as a path parameter
+        method: "GET",
+    }).then(response => {
+        return response.json();                         //return response to JSON
+    }).then(response => {                                   //something here
+        if (response.hasOwnProperty("Error")) {         //checks if response from server has an "Error"
+            alert(JSON.stringify(response));            // if it does, convert JSON object to string and alert
+        } else {
+            document.getElementById("DisplayOneUser").innerHTML = response.UserID + " " + response.UserName;  //output data
+        }
+    });
+}
 
 
 
