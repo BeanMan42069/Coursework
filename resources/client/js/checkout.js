46ec3a53a70af455aeb43a50b60fc70f9f9bfec9
92023 <92023@farnborough.ac.uk>
2020-12-04 16:07:44 +0000

    Added checkout.html, with a button that allows data to be retrieved from a basket using a userID > OrderID > data into table, need to add form on the html site to allow for userid to be entered, also need to update to take the userid from cookie token when logged in

---
+++ resources/client/js/checkout.js
@@ -0,0 +1,43 @@
+
+function getBasket() {
+    //debugger;
+    console.log("Invoked getBasket()");     //console.log your BFF for debugging client side
+    //const UserID = document.getElementById("UserID").value;  //get the UserId from the HTML element with id=UserID
+    let UserID = 1; 			  //You could hard code it if you have problems
+    //debugger;				  //debugger statement to allow you to step through the code in console dev F12
+    //const url = "/checkout/get/";       // API method on webserver
+    let OrderID = 0;
+    //fetch(url + UserID, {                // UserID as a path parameter
+    //    method: "GET",
+    //}).then(response => {
+    //    return response.json();                         //return response to JSON
+    //}).then(response => {                                   //something here
+     //   if (response.hasOwnProperty("Error")) {         //checks if response from server has an "Error"
+     //       alert(JSON.stringify(response)b[);            // if it does, convert JSON object to string and alert
+     //   } else {
+            //document.getElementById("DisplayOneUser").innerHTML = response.UserID + " " + response.UserName;  //output data
+      //      OrderID = response.OrderID;
+    //    }
+    //});
+    debugger;
+    console.log(UserID);
+    const url1 = "/checkout/getItems/";
+    let itemsHTML = "";
+    fetch(url1 + UserID, {                // UserID as a path parameter
+        method: "GET",
+    }).then(response => {
+        return response.json();                         //return response to JSON
+    }).then(response => {                                   //something here
+        if (response.hasOwnProperty("Error")) {         //checks if response from server has an "Error"
+            alert(JSON.stringify(response));            // if it does, convert JSON object to string and alert
+        } else {
+            for (let items of response) {
+
+              itemsHTML += items.name + " " + items.UserId;
+            }
+        }
+        document.getElementById("DisplayOneUser").innerHTML = itemsHTML;  //output data
+        //alert(JSON.stringify(response));
+    });
+
+}
---
+++ src/main/java/controllers/checkout.java
@@ -0,0 +1,78 @@
+package controllers;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import server.Main;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@Path("checkout/")
+@Consumes(MediaType.MULTIPART_FORM_DATA)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class checkout {  //get basket NEED TO UPDATE FOR ADDED table
+    @GET
+    @Path("get/{UserID}")
+    public String GetUser(@PathParam("UserID") Integer UserID) {
+        System.out.println("Invoked Users.GetUser() with UserID " + UserID);
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT OrderID FROM Orders WHERE UserID = ?"); //selecting UserID and Name from the table Users where the UserID is x
+            ps.setInt(1, UserID);
+            ResultSet results = ps.executeQuery();
+            JSONObject response = new JSONObject();
+            int OrderId = 0;
+            if (results.next() == true) {
+                response.put("OrderID", results.getString(1)); //fetching OrderID
+                OrderId = results.getInt(1);
+
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+    @GET
+    @Path("getItems/{UserID}")
+    public String GetItems(@PathParam("UserID") Integer UserID) {
+        System.out.println("Invoked Users.GetUser() with UserID " + UserID);
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT OrderID FROM Orders WHERE UserID = ?"); //selecting UserID and Name from the table Users where the UserID is x
+            ps.setInt(1, UserID);
+            ResultSet results = ps.executeQuery();
+            //JSONObject response = new JSONObject();
+            int orderID = 0;
+            if (results.next() == true) {
+                orderID = results.getInt(1);
+                System.out.println( orderID + "OrderID");
+            }
+            PreparedStatement ps1 = Main.db.prepareStatement("Select  items.name from orders, items, added where orders.orderid = added.orderid and added.ItemID = items.itemId and orders.orderid = ?;"); //selecting UserID and Name from the table Users where the UserID is x
+            ps1.setInt(1, orderID);
+            ResultSet results1 = ps1.executeQuery();
+            System.out.println(results1.getString(1));
+            JSONArray response = new JSONArray();
+
+            while (results1.next() == true) {
+                System.out.println("userid "+ UserID);
+                System.out.println("name "+ results1.getString(1));
+                JSONObject row1 = new JSONObject();
+                row1.put("UserId", UserID);
+                row1.put("name", results1.getString(1));
+                response.add(row1);
+
+            }
+
+            //if (results.next() == true) {
+               // response.put("OrderID", results.getInt(1)); //fetching UserID
+           // }
+
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+}
